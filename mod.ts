004d1ab4bc6070a0f9a236bc2ea04149787b894f
Holden Malinchock <h.malinchock@verizon.net>
2024-10-24 22:30:54 -0400

    Sending alpaca Request with url library

---
+++ mod.ts
@@ -0,0 +1,23 @@
+export const ALPACA_BASE_URL_PAPER = "https://paper-api.alpaca.markets/v2"
+export const ALPACA_BASE_URL_REAL = "https://api.alpaca.markets/v2"
+export const ALPACA_BASE_URL_DATA = "https://data.alpaca.markets/v2"
+
+/**
+ * Sends an alpaca request to the alpaca api.  Depends on the alpaca api key and secret key being set in the environment variables.
+ *
+ * @param url The url string
+ * @param method The method of the request (GET, POST, DELETE, etc)
+ * @param body The body of the request
+ * @returns a promise with the response from the alpaca api
+ */
+export const sendAlpacaRequest = async (url: string, method: string, body: any) => {
+    return await fetch(url, {
+        method: method,
+        body: body,
+        headers: {
+            "APCA-API-KEY-ID": Deno.env.get("ALPACA_API_KEY") ?? "",
+            "APCA-API-SECRET-KEY": Deno.env.get("ALPACA_SECRET_KEY") ?? "",
+            accept: 'application/json'
+        }
+    })
+}
